844613a0f9012c9027824955c5340dff991d5183
Nicolas Biri <nicolas@biri.name>
2015-07-10 12:17:07 +0200

    Add Haskell embedded DSL

---
+++ embeddeds/haskell/sample/Main.hs
@@ -0,0 +1,38 @@
+module Main where
+
+import ArduinoHS
+
+offline :: String
+offline = "offline"
+
+online :: String
+online = "online"
+
+button :: String
+button = "button"
+
+light :: String
+light = "light"
+
+on :: Signal
+on = High
+
+off :: Signal
+off = Low
+
+pushed :: Signal
+pushed = High
+
+released :: Signal
+released = Low
+
+main :: IO ()
+main = print $ buildApp "example" $ do
+  addSensor button $ onPort 9
+  addActuator light $ onPort 12
+  defineState offline
+  defineState online
+  actionsWhen offline `execute` [ set light `to` off ]
+  actionsWhen online  `execute` [ set light `to` on ]
+  transitionsFrom online  `are` [ when button `is` pushed $ goto offline ]
+  transitionsFrom offline `are` [ when button `is` released $ goto online ]
---
+++ embeddeds/haskell/src/ArduinoHS.hs
@@ -0,0 +1,6 @@
+module ArduinoHS (
+  module A
+  ) where
+
+import ArduinoHS.DSL as A
+import ArduinoHS.Model as A
