75173ff5b388d4a9983d8ba6f491f2f6cd7281f2
Pascal Poizat <pascal.poizat@lip6.fr>
2015-07-11 18:50:53 +0200

    update of the model classes

--- embeddeds/python/State.py
+++ embeddeds/python/State.py
@@ -9,13 +9,13 @@ class State(NamedElement):
 
     """
 
-    def __init__(self, name, transition=None, actions=[]):
+    def __init__(self, name, actions=[], transition=None):
         """
         Constructor.
 
         :param name: String, name of the state
-        :param transition: Transition, unique outgoing transition (should be 1)
         :param actions: List[Action], sequence of actions to do when entering the state (size should be > 0)
+        :param transition: Transition, unique outgoing transition
         :return:
         """
         NamedElement.__init__(self, name)
