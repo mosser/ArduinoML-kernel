c036e707b9ee0e1300fb116153754fd20aba21e6
julien deantoni <julien.deantoni@inria.fr>
2021-01-20 15:10:24 +0100

    new FSM code strategy based on switch case to prevent arduino simulator from hanging/crashing

--- kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/generator/ToWiring.java
+++ kernels/jvm/src/main/java/io/github/mosser/arduinoml/kernel/generator/ToWiring.java
@@ -9,81 +9,92 @@ import io.github.mosser.arduinoml.kernel.structural.*;
  */
 public class ToWiring extends Visitor<StringBuffer> {
 
-	private final static String CURRENT_STATE = "current_state";
-
 	public ToWiring() {
 		this.result = new StringBuffer();
 	}
 
 	private void w(String s) {
-		result.append(String.format("%s\n",s));
+		result.append(String.format("%s",s));
 	}
 
 	@Override
 	public void visit(App app) {
-		w("// Wiring code generated from an ArduinoML model");
-		w(String.format("// Application name: %s\n", app.getName()));
+		w("// Wiring code generated from an ArduinoML model\n");
+		w(String.format("// Application name: %s\n", app.getName())+"\n");
 
-		w("void setup(){");
+		w("long debounce = 200;\n");
+		w("\nenum STATE {");
+		String sep ="";
+		for(State state: app.getStates()){
+			w(sep+state.getName());
+			sep=", ";
+		}
+		w("};\n");
+		if (app.getInitial() != null) {
+			w("STATE currentState = " + app.getInitial().getName()+";\n");
+		}
+
+		for(Brick brick: app.getBricks()){
+			if (brick instanceof Sensor){ //it may be a button so we may debounce
+				w(String.format("\nboolean %sBounceGuard = false;\n", brick.getName()));
+				w(String.format("long %sLastDebounceTime = 0;\n", brick.getName()));
+			}
+		}
+		w("\nvoid setup(){\n");
 		for(Brick brick: app.getBricks()){
 			brick.accept(this);
 		}
 		w("}\n");
 
-		w("long time = 0; long debounce = 200;\n");
-
+		w("\nvoid loop() {\n" +
+			"\tswitch(currentState){\n");
 		for(State state: app.getStates()){
 			state.accept(this);
 		}
-
-		if (app.getInitial() != null) {
-			w("void loop() {");
-			w(String.format("  state_%s();", app.getInitial().getName()));
-			w("}");
-		}
+		w("\t}\n\t  delay(debounce); //enable the FSM periodically to avoid bouncing effects\n" +
+			"}");
 	}
 
 	@Override
 	public void visit(Actuator actuator) {
-	 	w(String.format("  pinMode(%d, OUTPUT); // %s [Actuator]", actuator.getPin(), actuator.getName()));
+	 	w(String.format("  pinMode(%d, OUTPUT); // %s [Actuator]\n", actuator.getPin(), actuator.getName()));
 	}
 
 
 	@Override
 	public void visit(Sensor sensor) {
-		w(String.format("  pinMode(%d, INPUT);  // %s [Sensor]", sensor.getPin(), sensor.getName()));
+		w(String.format("  pinMode(%d, INPUT);  // %s [Sensor]\n", sensor.getPin(), sensor.getName()));
 	}
 
 	@Override
 	public void visit(State state) {
-		w(String.format("void state_%s() {",state.getName()));
+		w("\t\tcase "+state.getName()+":\n");
 		for(Action action: state.getActions()) {
 			action.accept(this);
 		}
 
 		if (state.getTransition() != null) {
-			w("  boolean guard = millis() - time > debounce;");
-			context.put(CURRENT_STATE, state);
 			state.getTransition().accept(this);
-			w("}\n");
+			w("\t\tbreak;\n");
 		}
 
 	}
 
 	@Override
 	public void visit(Transition transition) {
-		w(String.format("  if( digitalRead(%d) == %s && guard ) {",
+		String sensorName = transition.getSensor().getName();
+		w(String.format("\t\t\t%sBounceGuard = millis() - %sLastDebounceTime > debounce;\n",
+				sensorName, sensorName));
+		w(String.format("\t\t\tif( digitalRead(%d) == %s) {\n",
 				transition.getSensor().getPin(),transition.getValue()));
-		w("    time = millis();");
-		w(String.format("    state_%s();",transition.getNext().getName()));
-		w("  } else {");
-		w(String.format("    state_%s();",((State) context.get(CURRENT_STATE)).getName()));
-		w("  }");
+		w(String.format("\t\t\t\t%sLastDebounceTime = millis();\n", sensorName));
+		w("\t\t\t\tcurrentState = "+transition.getNext().getName()+";\n");
+		w("\t\t\t}\n");
 	}
 
 	@Override
 	public void visit(Action action) {
-		w(String.format("  digitalWrite(%d,%s);",action.getActuator().getPin(),action.getValue()));
+		w(String.format("\t\t\tdigitalWrite(%d,%s);\n",action.getActuator().getPin(),action.getValue()));
 	}
 
 }
