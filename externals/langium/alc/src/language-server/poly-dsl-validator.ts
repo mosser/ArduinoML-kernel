19ae0886cc58252e01be627d1591cd12180a236a
I553266 <armand.fargeon@sap.com>
2022-01-27 16:37:40 +0100

    add langium

---
+++ externals/langium/alc/src/language-server/poly-dsl-validator.ts
@@ -0,0 +1,38 @@
+import { ValidationAcceptor, ValidationCheck, ValidationRegistry } from 'langium';
+import { PolyDslAstType, Person } from './generated/ast';
+import { PolyDslServices } from './poly-dsl-module';
+
+/**
+ * Map AST node types to validation checks.
+ */
+type PolyDslChecks = { [type in PolyDslAstType]?: ValidationCheck | ValidationCheck[] }
+
+/**
+ * Registry for validation checks.
+ */
+export class PolyDslValidationRegistry extends ValidationRegistry {
+    constructor(services: PolyDslServices) {
+        super(services);
+        const validator = services.validation.PolyDslValidator;
+        const checks: PolyDslChecks = {
+            Person: validator.checkPersonStartsWithCapital
+        };
+        this.register(checks, validator);
+    }
+}
+
+/**
+ * Implementation of custom validations.
+ */
+export class PolyDslValidator {
+
+    checkPersonStartsWithCapital(person: Person, accept: ValidationAcceptor): void {
+        if (person.name) {
+            const firstChar = person.name.substring(0, 1);
+            if (firstChar.toUpperCase() !== firstChar) {
+                accept('warning', 'Person name should start with a capital.', { node: person, property: 'name' });
+            }
+        }
+    }
+
+}
